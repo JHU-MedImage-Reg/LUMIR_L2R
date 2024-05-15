63705137ccb0141b8a8ef08e13e42795156a6d8c
Junyu Chen <jchen245@jhmi.edu>
2024-05-15 14:26:24 -0400

    Update datasets.py to include JSON parsing capability.

--- TransMorph/data/datasets.py
+++ TransMorph/data/datasets.py
@@ -1,7 +1,6 @@
 import os, glob
-import torch, sys
+import torch, sys, json
 from torch.utils.data import Dataset
-from .data_utils import pkload
 import matplotlib.pyplot as plt
 import random
 import numpy as np
@@ -44,3 +43,43 @@ class L2RLUMIRDataset(Dataset):
 
     def __len__(self):
         return len(self.paths)
+
+class L2RLUMIRJSONDataset(Dataset):
+    def __init__(self, base_dir, json_path, stage='train'):
+        with open(json_path) as f:
+            d = json.load(f)
+        if stage.lower()=='train':
+            self.imgs = d['training']
+        elif stage.lower()=='validation':
+            self.imgs = d['validation']
+        else:
+            raise 'Not implemented!'
+        self.base_dir = base_dir
+        self.stage = stage
+
+    def __getitem__(self, index):
+        if self.stage == 'train':
+            mov_dict = self.imgs[index]
+            fix_dicts = self.imgs.copy()
+            fix_dicts.remove(mov_dict)
+            random.shuffle(fix_dicts)
+            fix_dict = fix_dicts[0]
+            x = nib.load(self.base_dir+mov_dict['image'])
+            y = nib.load(self.base_dir+fix_dict['image'])
+
+        else:
+            img_dict = self.imgs[index]
+            mov_path = img_dict['moving']
+            fix_path = img_dict['fixed']
+            x = nib.load(self.base_dir + mov_path)
+            y = nib.load(self.base_dir + fix_path)
+        x = x.get_fdata() / 255.
+        y = y.get_fdata() / 255.
+        x, y = x[None, ...], y[None, ...]
+        x = np.ascontiguousarray(x)  # [channels,Height,Width,Depth]
+        y = np.ascontiguousarray(y)
+        x, y = torch.from_numpy(x), torch.from_numpy(y)
+        return x.float(), y.float()
+
+    def __len__(self):
+        return len(self.imgs)
